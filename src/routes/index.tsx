232ccd7c192dacca9fabddbbbae74974d7991e44
Kauã Cantanhede <99539079+kant-sdev@users.noreply.github.com>
2026-07-01 17:56:23 +0000

    cc

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -243,7 +243,7 @@ function HomePage() {
                 rel="noreferrer"
                 className="inline-flex items-center justify-center rounded-full border border-border bg-background px-6 py-3 text-sm font-semibold text-foreground hover:bg-muted transition-all"
               >
-                <YouTubeIcon className="size-4 px-6" />
+                <YouTubeIcon className="size-4 pr-2" />
 
                 Ir para Youtube
               </a>
