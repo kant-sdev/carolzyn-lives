be74fa87cbaa7be378786fb1fc8dd24a8abc7cc9
Kauã Cantanhede <99539079+kant-sdev@users.noreply.github.com>
2026-07-30 20:38:33 +0000

    auteração do Head

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -76,7 +76,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "carolzyn - um cantinho cozy para os filhotes" },
+      { title: "Carolzyn - um cantinho cozy para os filhotes" },
       {
         name: "description",
         content:
